b86bb9d158303d1503096d43022a259f5fd6f6d0
vilnis <vjatnieks@loyola.edu>
2025-03-25 12:54:51 -0400

    Functional filter

--- index.ts
+++ index.ts
@@ -7,6 +7,12 @@ interface Student{
   gpa: number;
   status: string;
 }
+
+interface Student{
+    gpa: number
+
+}
+
 // Interface for a course
 interface Course{
   id: string;
@@ -18,7 +24,7 @@ interface Course{
 // List of mixed entries
 type entry = Course | Student;
 const mixedList: entry[] = [  {id: 1, name: "Alice", gpa: 3.8, status: "active"},
-                            {id: 2, name: "Bob", gpa: 3.2, status: "active"},
+                            {id: 2, name: "Bob", gpa: 3.2, status: "paused"},
                             {id: "CS451", title: "Programming Languages", credits: 3},
                             {id: "TS101", title: "Intro to TypeScript", credits: 1}
                          ];
@@ -26,17 +32,31 @@ console.log("Mixed list", mixedList);
 
 
 // Find list of students only
-function isStudent(obj: entry): obj is Student {
+function isStudent(obj: entry):obj is any {
     return "gpa" in obj;
 }
 const studentList: Student[] = mixedList.filter(isStudent);
 console.log("Student list", studentList);
 
-
 // Find list of courses only
-function isCourse(obj: entry): obj is Course {
+function isCourse(obj: entry){
     return "credits" in obj;
 }
 const courseList: Course[] = mixedList.filter(isCourse);
 console.log("Course list", courseList);
+
+
+
+// functional way of doing it
+// is there some way to remove !("gpa" in list[0]) so that we can pass in a function that defines whay type of object we are filtering
+type filterFunc = (list: entry[]) => entry[];
+const filterListStudent: filterFunc = list =>{
+    if(list.length === 0) return [];
+    if(!("gpa" in list[0])) return filterListStudent(list.slice(1));
+    else return [list[0], ...filterListStudent(list.slice(1))];
+}
+
+const studentList2: entry [] = filterListStudent(mixedList);
+console.log("Student list2", studentList2);
+
 
