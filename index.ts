f589344af185a29d852990623241a34875cc8bba
vilnis <vjatnieks@loyola.edu>
2025-03-25 13:30:35 -0400

    is student and is course are now functional

--- index.ts
+++ index.ts
@@ -7,12 +7,6 @@ interface Student{
   gpa: number;
   status: string;
 }
-
-interface Student{
-    gpa: number
-
-}
-
 // Interface for a course
 interface Course{
   id: string;
@@ -21,9 +15,11 @@ interface Course{
 }
 
 
+
+
 // List of mixed entries
-type entry = Course | Student;
-const mixedList: entry[] = [  {id: 1, name: "Alice", gpa: 3.8, status: "active"},
+type Entry = Course | Student;
+const mixedList: Entry[] = [  {id: 1, name: "Alice", gpa: 3.8, status: "active"},
                             {id: 2, name: "Bob", gpa: 3.2, status: "paused"},
                             {id: "CS451", title: "Programming Languages", credits: 3},
                             {id: "TS101", title: "Intro to TypeScript", credits: 1}
@@ -31,32 +27,48 @@ const mixedList: entry[] = [  {id: 1, name: "Alice", gpa: 3.8, status: "active"}
 console.log("Mixed list", mixedList);
 
 
-// Find list of students only
-function isStudent(obj: entry):obj is any {
+
+
+/*  purpose: set up the structure of our is a what? functions   */
+type isFunc = (obj: Entry) => boolean;
+
+/*  purpose: determine if an Entry is a student    */
+const isStudent: isFunc = obj => {
     return "gpa" in obj;
 }
-const studentList: Student[] = mixedList.filter(isStudent);
-console.log("Student list", studentList);
 
-// Find list of courses only
-function isCourse(obj: entry){
-    return "credits" in obj;
+/* purpose: determine if an Entry is a course        */
+const isCourse: isFunc = obj =>{
+    return "title" in obj;
 }
-const courseList: Course[] = mixedList.filter(isCourse);
+
+// functional way of doing it
+// is there some way to remove isStudent so that we can pass in a function that defines whay type of object we are filtering
+type filterFunc = (list: Entry[]) => Entry[];
+const filterListStudent: filterFunc = list =>{
+    if(list.length === 0) return [];
+    if(!isStudent(list[0])) return filterListStudent(list.slice(1));
+    else return [list[0], ...filterListStudent(list.slice(1))];
+}
+
+const filterListCourse: filterFunc = list =>{
+    if(list.length === 0) return [];
+    if(!isCourse(list[0])) return filterListCourse(list.slice(1));
+    else return [list[0], ...filterListCourse(list.slice(1))];
+}
+
+
+
+/* purpose: compute and output our results  */
+const studentList: Entry[] = filterListStudent(mixedList);
+const courseList: Entry[] = filterListCourse(mixedList);
+
+console.log("Student list2", studentList);
 console.log("Course list", courseList);
 
 
 
-// functional way of doing it
-// is there some way to remove !("gpa" in list[0]) so that we can pass in a function that defines whay type of object we are filtering
-type filterFunc = (list: entry[]) => entry[];
-const filterListStudent: filterFunc = list =>{
-    if(list.length === 0) return [];
-    if(!("gpa" in list[0])) return filterListStudent(list.slice(1));
-    else return [list[0], ...filterListStudent(list.slice(1))];
-}
 
-const studentList2: entry [] = filterListStudent(mixedList);
-console.log("Student list2", studentList2);
+
 
 
