026f71727446f1bc880a4ae231bbb6eaf277233a
vilnis <vjatnieks@loyola.edu>
2025-03-25 20:39:28 -0400

    comments

---
+++ random.ts
@@ -0,0 +1,34 @@
+
+//github token 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
+
+type stringOrNum = string | number;
+
+
+type Animal = {
+    name: string;
+    height: number;
+    weight: number;
+}
+
+interface Student{
+    name: string;
+}
+
+interface Student{
+    gpa: number;
+}
+
+
+interface person {
+    name: string;
+    age: number
+}
+
+function printPerson (p : person){
+    console.log(`Name: ${p.name} \n Age: ${p.age}` );
+}
+
+let alice: person = {name: "Alice", age: 25};
+printPerson(alice);
+
+printPerson({name: "John", age: 47});
